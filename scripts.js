c3fe59536459624c7dbe9de0e681dad14e5ced3e
leandromacedosilva <iamfulldeveloper@gmail.com>
2025-09-03 09:16:21 -0300

    update file styles.css, scripts.js and index.html

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 function enviarWhats(event) {
   event.preventDefault();
 
-  const nome = window.document.getElementById("nome").vavlue;
-  const mensagem = window.document.getElementById("mensagem").vavlue;
-  const telefone  = '';
+  const nome = window.document.getElementById("nome").value;
+  const mensagem = window.document.getElementById("mensagem").value;
+  const telefone = "55949917xxxx";
   const texto = `Olá, chamo-me ${nome}, ${mensagem}.`;
   const msgFormatada = encodeURIComponent(texto);
   const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
 
   console.log(url);
 
-  window.open(url, '_blank');
-
+  window.open(url, "_blank");
 }
